ff0e0ebecd392a321d9f3391b5f400545b5b934b
Arthur Piminov <A.E.Piminov@outlook.com>
2023-10-04 18:50:04 +0700

    lab 2.1 done, TODO unit tests

--- src/clojure_lab_2/core.clj
+++ src/clojure_lab_2/core.clj
@@ -1,7 +1,33 @@
 (ns clojure-lab-2.core)
 
-(defn foo
-  [x]
-  (println x "Hello, World!"))
+(defn cube [x] (* x x x))
+(defn e [x] (* x x x))
 
- (foo "a")
+
+(defn fact [xstart] (loop [acc 1
+                      x xstart]
+                 (if (zero? x)
+                  acc 
+                   (recur (* acc x) (dec x)))))
+
+(cube 3)
+
+(defn sum [term a nxt b]
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (nxt a) nxt b))))
+
+(defn integral [f a b n]
+  (let [h (/ (- b a) n)
+        yk (fn [k] (f (+ a (* h k))))
+        trapezoid-term (fn [k] (*
+                              (cond
+                                (or (= k 0) (= k n)) 1
+                                :else 2)
+                              (yk k)))]
+    (* (/ h 2) ((memoize sum) trapezoid-term 0 inc n))))
+
+(time (integral cube 0 3 100))
+(time (integral #(Math/exp %) 1 1000 100))
+
