3873f932717ada8b9f596a7d54a436cc1c8c4a43
Arthur Piminov <A.E.Piminov@outlook.com>
2023-10-11 18:54:53 +0700

    renamed lab2_1

--- src/clojure_lab_2/lab2_1.clj
+++ src/clojure_lab_2/lab2_1.clj
@@ -1,35 +1,30 @@
 (ns clojure-lab-2.core)
 
-; memoize function
-
 (defn cube [x] (* x x x))
-(defn e [x] (* x x x))
 
+(def memoize-cube (memoize cube))
 
-(defn fact [xstart] (loop [acc 1
-                      x xstart]
-                 (if (zero? x)
-                  acc 
-                   (recur (* acc x) (dec x)))))
-
-(cube 3)
-
-(defn sum [term a nxt b]
-  (if (> a b)
-    0
-    (+ (term a)
-       (sum term (nxt a) nxt b))))
+(defn sum-iterate [term a nxt b]
+  (loop [acc 0
+         a a]
+    (if (> a b)
+      acc
+      (recur (+ acc (term a)) (nxt a)))))
 
 (defn integral [f a b n]
-  (let [h (/ (- b a) n)
+  (let [h (double (/ (- b a) n))
         yk (fn [k] (f (+ a (* h k))))
         trapezoid-term (fn [k] (*
-                              (cond
-                                (or (= k 0) (= k n)) 1
-                                :else 2)
-                              (yk k)))]
-    (* (/ h 2) ((memoize sum) trapezoid-term 0 inc n))))
+                                (cond
+                                  (or (= k 0) (= k n)) 1
+                                  :else 2)
+                                (yk k)))]
+    (* (/ h 2) (memoize (sum-iterate trapezoid-term 0 inc n)))))
 
-(time (integral cube 0 3 100))
-(time (integral #(Math/exp %) 1 1000 100))
+(def memoize-integral (memoize integral))
 
+(do 
+(time (integral memoize-cube 2 10 10000))
+(time (memoize-integral memoize-cube 1 10 10000))
+(time (integral #(Math/exp %) 1 7 100))
+)
