025123334dc570159db0d404efd321ee77dfdecc
Arthur Piminov <artyr26@hotmail.com>
2023-10-11 14:58:25 +0700

    added tests

--- test/clojure_lab_2/core_test.clj
+++ test/clojure_lab_2/core_test.clj
@@ -1,7 +1,17 @@
 (ns clojure-lab-2.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-lab-2.core :refer :all]))
+  (:require
+   [clojure.test :refer :all]
+   [clojure-lab-2.core :refer :all]
+   ))
 
-(deftest a-test
+(deftest integration-test
+  (defn square [x] (* x x))
+  (defn sqrt [x] (Math/sqrt x))
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 72 (Math/round (integral square 0 6 100))))
+    (is (= 9597 (Math/round (integral square 10 31 100))))
+    (is (= 21 (Math/round (integral sqrt 0 10 100))))
+    (is (= 7453 (Math/round (integral sqrt 0 500 200))))
+    ))
+
+(run-tests 'clojure-lab-2.core-test)
