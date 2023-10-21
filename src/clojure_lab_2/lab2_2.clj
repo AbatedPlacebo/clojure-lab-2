0f5d5cefb64ad95a50d7a1071ad23ee655fd068c
Arthur Piminov <artyr26@hotmail.com>
2023-10-21 12:54:26 +0700

    lab 2.2 done

--- src/clojure_lab_2/lab2_2.clj
+++ src/clojure_lab_2/lab2_2.clj
@@ -2,13 +2,10 @@
 
 (defn cube [x] (* x x x))
 
-(defn sum-lazy [term a nxt]
+(defn sum-lazy [term a nxt h]
   (lazy-seq
-   (cons (term a) (sum-lazy term (nxt a) nxt))))
+   (cons (* (/ h 2) (term a)) (sum-lazy term (nxt a) nxt h))))
 
-(defn sum-ready-lazy [n h term]
-  (lazy-seq (cons (* ( / h 2) (reduce + (take (+ n 1) (sum-lazy term 0 inc)))) (sum-ready-lazy n h term)))
-  )
 
 (defn lazy-integral [f a b n]
   (let [h (double (/ (- b a) n))
@@ -18,9 +15,15 @@
                                   (or (= k 0) (= k n)) 1
                                   :else 2)
                                 (yk k)))]
-   (take 1 (sum-ready-lazy n h trapezoid-term))
+   (sum-lazy trapezoid-term 0 inc h) 
     ))
 
-(time (lazy-integral cube 0 3 10))
-(time (integral cube 0 3 100))
-(time (lazy-integral #(Math/exp %) 2 6 100))
+(defn integral-reductions [f a b n]
+ (reductions + (lazy-integral f a b n)) 
+  )
+
+(let [ls (integral-reductions cube 0 3 100000)] 
+(println (nth ls 100000))
+(println (nth ls (dec 100000)))
+(println (nth ls (inc 100000)))
+)
